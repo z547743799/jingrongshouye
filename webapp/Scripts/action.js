541453c175f999a5693e5cda54c2459222151209
lzw <lzw@lzw.localdomain>
2020-10-05 20:49:37 +0800

    --

--- webapp/Scripts/action.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-	 $('.banner #z-ulImageBox li').soChange({
-            changeTime: 6500,
-            thumbNowClass: 'sindex',
-            thumbObj: '#z-olImageIndex li',
-            overStop: true
-        });
-
-})
